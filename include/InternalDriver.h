a709f76894bc37559b36dde90764dc7b7a7ba0b3
Giacomo <64547453+GDP01@users.noreply.github.com>
2025-11-19 11:55:11 +0100

    Update InternalDriver.h

--- include/InternalDriver.h
+++ include/InternalDriver.h
@@ -6,19 +6,19 @@ const int BUFFER_DIM = 200;
 class InertialDriver
 {
     private:
-    //vector<MyVector> arr = new vector<MyVector>[BUFFER_DIM];
-
-    MyVector<misura> measureBuffer; 
-    int front;
-    int back;
-    int count;
+        MyVector<misura> measureBuffer; 
+        int front;
+        int back;
+        int count;
 
     public:
-    InertialDriver(); //costruttore
-    void push_back(const misura& m);
-    misura pop_front();
-    void clear_buffer();
-    lettura get_reading(int sensor_index) const;
+        InertialDriver(); //costruttore
+        ~InertialDriver();
+        void push_back(const misura& m);
+        misura pop_front();
+        void clear_buffer();
+        lettura get_reading(int sensor_index) const;
+        std::ostream& operator<<(std::ostream& os, const InertialDriver& driver);
 };
 #endif
 
