c72433078db1d7e8df4b2a45131c6360b4baa7ee
Giacomo <64547453+GDP01@users.noreply.github.com>
2025-11-19 11:57:12 +0100

    Clean up InternalDriver.h by removing comments
    Removed commented-out function descriptions and unused preprocessor directive.

--- include/InternalDriver.h
+++ include/InternalDriver.h
@@ -20,15 +20,3 @@ class InertialDriver
         lettura get_reading(int sensor_index) const;
         std::ostream& operator<<(std::ostream& os, const InertialDriver& driver);
 };
-#endif
-
-//La funzione push_back che accetta un array stile C contenente una misura e la
-//memorizza nel buffer (sovrascrivendo la misura meno recente se il buffer è pieno);
-
-//● La funzione pop_front che fornisce in output un array stile C contenente la misura più
-//vecchia e la rimuove dal buffer;
-//● La funzione clear_buffer che elimina (senza restituirle) tutte le misure salvate;
-//● La funzione get_reading che accetta un numero tra 0 e 16 e ritorna la corrispondente
-//lettura della misura più recente, senza cancellarla dal buffer;
-//● L’overloading dell’operator<< che stampa a schermo l’ultima misura salvata (ma non la
-//rimuove dal buffer)
