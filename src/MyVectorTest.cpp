627ced95302b728bc9469e59825743d7faab0e33
Giacomo <64547453+GDP01@users.noreply.github.com>
2025-11-19 11:20:51 +0100

    Tester di MyVector

--- src/MyVectorTest.cpp
+++ src/MyVectorTest.cpp
@@ -1,27 +1,35 @@
 #include "MyVector.h"
 #include <iostream>
 
-void arrayPrint(double a[], int length, int size){	
-	std::cout << "Array: [";
-	for(int i=0; i<length; i++){
-		std::cout << a[i];
+void arrayPrint(const double a[], int length, int size) {
+    std::cout << "Array: [";
+    for (int i = 0; i < length; ++i) {
+        std::cout << a[i];
 
         if (i == (size - 1)) {
-            std::cout  << "|";
+            std::cout << "|";
         }
-		
-		if(i < (length-1)){
-			std::cout << ", ";
-		} else {
-			std::cout << "]" << std::endl;
-		}
-	}
+
+        if (i < (length - 1)) {
+            std::cout << ", ";
+        } else {
+            std::cout << "]" << std::endl;
+        }
+    }
 }
 
 int main() {
-    MyVector a = MyVector(5);
+    // uso esplicito del template con double
+    MyVector<double> a(5);
 
     a[3] = 2.4;
+    std::cout << "a[3] = " << a[3] << std::endl;
+
+    a.push_back(10.5);
+    std::cout << "size = " << a.size() << std::endl;
+
+    double last = a.pop_back();
+    std::cout << "pop_back() = " << last << ", size = " << a.size() << std::endl;
 
     return 0;
 }
