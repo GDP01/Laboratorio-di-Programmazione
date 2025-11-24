60d60a42372163ee69840d461d92e24b33c94323
Giacomo <64547453+GDP01@users.noreply.github.com>
2025-11-24 09:59:02 +0100

    Add const qualifier to getter methods in lettura

--- src/lettura.cpp
+++ src/lettura.cpp
@@ -8,28 +8,27 @@ lettura::lettura(double yv, double ya, double pv, double pa, double rv, double r
 : yaw_v(yv), yaw_a(ya), pitch_v(pv), pitch_a(pa), roll_v(rv), roll_a(ra)
 {}
 
-double lettura::get_yaw_v()
+double lettura::get_yaw_v() const
 {
     return yaw_v;
 }
-double lettura::get_yaw_a()
+double lettura::get_yaw_a() const
 {
     return yaw_a;
 }
-double lettura::get_pitch_v()
+double lettura::get_pitch_v() const
 {
     return pitch_v;
 }
-double lettura::get_pitch_a()
+double lettura::get_pitch_a() const
 {
     return pitch_a;
 }
-double lettura::get_roll_v()
+double lettura::get_roll_v() const
 {
     return roll_v;
 }
-double lettura::get_roll_a()
+double lettura::get_roll_a() const
 {
     return roll_a;
-
 }
