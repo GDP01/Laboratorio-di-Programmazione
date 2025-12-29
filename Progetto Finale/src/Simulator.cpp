6bdf5028a2f0cafe733e0a3f5bb0bc263a1f5f26
Andrea Vadori <118353413+Ellocovado@users.noreply.github.com>
2025-12-29 01:41:58 +0100

    Add methods pickRandomEntry and pickRandomExit

--- Progetto Finale/src/Simulator.cpp
+++ Progetto Finale/src/Simulator.cpp
@@ -1,7 +1,7 @@
 // @author: Andrea Vadori
 
 #include "Simulator.h"
-//#include "Highway.h"
+#include "Highway.h"
 #include <iostream>
 #include <fstream>
 #include <iomanip>
@@ -17,8 +17,23 @@ double Simulator::generatoreTimeGap(){
     std::uniform_real_distribution<double> distanza(MIN_GAP_SECONDS, MAX_GAP_SECONDS);
     return distanza(gen);
 
+int Simulator::pickRandomEntry(const Highway& highway){
+    //Casting per size_t
+    int maxId=static_cast<int>(highway.interchangeCount());
+    //Evito di far entrare all'ultimo svincolo
+    if(maxId<2) return 1;
+    std::uniform_int_distribution<> dist(1, maxId-1);
+    return dist(gen);
+}
+
+int Simulator::pickRandomExit(const Highway& highway, int entryId){
+    int maxId=static_cast<int>(highway.interchangeCount());
+    //Evito di far uscire al primo svincolo
+    std::uniform_int_distribution<> dist(entryId+1, maxId);
+    return dist(gen);
+
 //TODO: finire simulaRun
-/*void Simulator::simulaRun(const std::string& fileRuns, const std::string& filePassaggi, Highway& highway){
+void Simulator::simulaRun(const std::string& fileRuns, const std::string& filePassaggi, Highway& highway){
     std::ifstream inCorse(fileRuns);
     std::ifstream inPassaggi(filePassaggi);
     std::ofstream outFile(fileRuns);
@@ -33,4 +48,3 @@ double Simulator::generatoreTimeGap(){
     for(int i = 0; i < veicoliTotali; ++i){
         Vehicle veicolo;
         veicolo.generatoreTarga(gen);
-*/
