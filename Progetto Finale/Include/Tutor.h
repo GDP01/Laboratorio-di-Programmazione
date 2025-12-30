46e8dc2376e04bc84dfe3b2e0b02b478c8499276
Andrea Vadori <118353413+Ellocovado@users.noreply.github.com>
2025-12-30 16:34:03 +0100

    Add Tutor class and methods

--- Progetto Finale/Include/Tutor.h
+++ Progetto Finale/Include/Tutor.h
@@ -1,1 +1,52 @@
+//@author: Andrea Vadori
+#ifndef TUTOR_H
+#define TUTOR_H
 
+#include "Highway.h"
+#include <string>
+#include <vector>
+#include <unordered_map> //struttura dati utilizzata
+
+//#include <unordered_map>:implementa una tabella Hash. Permette di accedere al dato quasi istantaneamente, (O(1))
+
+//Singolo passaggio letto dal file
+struct Transito{
+  int idVarco;
+  std::string targa;
+  double timestamp;
+};
+
+//Memo per sapere dove si trovava l'auto
+struct DatiVeicolo{
+  int idUltimoVarco;
+  double ultimoTempo;
+};
+
+class Tutor{
+  private:
+      std::vector<Transito> storicoTransiti; //tutti i dati
+      size_t indice;
+      double tempoAttuale;
+      std::unordered_map<std::string, DatiVeicolo> veicoliAttivi;
+
+      //variabili per statitiche(media, multe ecc...)
+      long conteggioMulte;
+      double sommaVelocita;
+      long conteggioMisurazioni;
+
+      //conteggio dei passaggi su ogni varco
+      std::unordered_map<int,int> statisticheVarchi;
+
+public:
+      Tutor();//costruttore
+
+      //carica file transiti
+      bool init(const std::string& nomeFile);
+      //Gestione temporale e controllo velocita
+      void updateTime(double nuovoIstante, const Highway& autostrada);
+      
+      void reset();
+      //Output riepilogo
+      void showStats();
+};
+#endif
