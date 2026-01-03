501f9371853165fb1d81aaf1d1a592af683d9228
Giacomo <64547453+GDP01@users.noreply.github.com>
2026-01-03 23:23:50 +0100

    Add Simulator.h header file for simulation functionality

---
+++ Progetto Finale/Include/Simulator.h
@@ -0,0 +1,23 @@
+//Autore : Giacomo Dal Poz appartenente al gruppo: "C++2.0" (collaboratori : Andrea Moratto e Andrea Vadori)
+
+#ifndef SIMULATOR_H
+#define SIMULATOR_H
+
+#include <vector>
+#include <fstream>
+
+#include "../Include/Highway.h"
+#include "../Include/Veicolo.h"
+
+class Simulator{
+  //calcola il tempo in cui un veicolo passa al varco di indice indexVarco
+  double tempoPassato(const std::vector<VarcoSvincolo>& varchi, int indexVarco, const Veicolo& veicolo);
+
+  //genera i file Runs e Passages a partire dalla mappa autostradale.
+  void outputGenerator(const Highway& hMap, std::ofstream& fileRuns, std::ofstream& filePassages);
+}
+
+#endif
+
+
+
