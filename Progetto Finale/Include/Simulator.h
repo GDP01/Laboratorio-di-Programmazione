c081abe0748bc022935c3ef26fccd4f2d183a4f4
Giacomo <64547453+GDP01@users.noreply.github.com>
2026-01-03 23:24:39 +0100

    Refactor Simulator class method declarations

--- Progetto Finale/Include/Simulator.h
+++ Progetto Finale/Include/Simulator.h
@@ -9,12 +9,11 @@
 #include "../Include/Highway.h"
 #include "../Include/Veicolo.h"
 
-class Simulator{
-  //calcola il tempo in cui un veicolo passa al varco di indice indexVarco
-  double tempoPassato(const std::vector<VarcoSvincolo>& varchi, int indexVarco, const Veicolo& veicolo);
+//calcola il tempo in cui un veicolo passa al varco di indice indexVarco
+double tempoPassato(const std::vector<VarcoSvincolo>& varchi, int indexVarco, const Veicolo& veicolo);
 
-  //genera i file Runs e Passages a partire dalla mappa autostradale.
-  void outputGenerator(const Highway& hMap, std::ofstream& fileRuns, std::ofstream& filePassages);
+//genera i file Runs e Passages a partire dalla mappa autostradale.
+void outputGenerator(const Highway& hMap, std::ofstream& fileRuns, std::ofstream& filePassages);
 }
 
 #endif
