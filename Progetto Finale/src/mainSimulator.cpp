dc497784f26376af7c680f6fc4fb6d1649ec363c
Giacomo <64547453+GDP01@users.noreply.github.com>
2026-01-03 21:33:22 +0100

    Add main function for highway simulation
    Implement main function for highway simulation with error handling for file operations.

---
+++ Progetto Finale/src/mainSimulator.cpp
@@ -0,0 +1,48 @@
+//Autore : Moratto Andrea appartenente al gruppo: "C++2.0" (collaboratore : Giacomo Dal Poz e Andrea Vadori)
+
+#include <iostream>
+#include <ctime>
+#include "../Include/Veicolo.h"
+#include "../Include/Highway.h"
+
+int main()
+{
+    srand(time(0));
+    try{
+    
+    Highway hMap("Highway.txt");      //creazione mappa autostradale
+        
+    std::ofstream fileRuns("Runs.txt");  //file di output per i viaggi
+    try{
+    if(!fileRuns.is_open())                                                     //gestione errore apertura file
+        throw std::invalid_argument("Errore ! ");
+    }
+    catch(const std::invalid_argument& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+
+    std::ofstream filePassages("Passages.txt");   //file di output per i passaggi ai varchi
+    try{
+    if(!filePassages.is_open())                                                     //gestione errore apertura file
+        throw std::invalid_argument("Errore ! ");
+    }
+    catch(const std::invalid_argument& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+    
+    outputGenerator(hMap, fileRuns, filePassages);  //generazione degli output: file dei viaggi e file dei passaggi ai varchi
+    fileRuns.close();       //chiusura del file dei viaggi
+    filePassages.close();   //chiusura del file dei passaggi ai varchi
+    }
+    catch(const std::invalid_argument& e)   //gestione errori mappa autostradale
+    {
+        std::cerr << e.what() << std::endl;
+        return -1;
+    }
+    
+    return 0;
+}
